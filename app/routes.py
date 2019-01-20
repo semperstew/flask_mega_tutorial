54d0e57a3eb19b9c3cf2ec23bf7bd22e322ff285
andrew.stewart <andrew.stewart@elderresearch.com>
2019-01-19 23:24:03 -0500

    Part II: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'semperstew'}
+    posts = [
+        {
+            'author': {'username': 'semperstew'},
+            'body': 'Beautiful day in the neighborhood!'
+        },
+        {
+            'author': {'username': 'kwhill'},
+            'body': 'I love watching hands make faces!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
